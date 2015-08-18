2690cba704979c5df0886f8d3d78880aa9a95353
clydeBruckman <jacklynchtds@gmail.com>
2015-08-18 09:17:40 -0400

    added to noise-cancel

--- noise-cancel-test1.cpp
+++ noise-cancel-test1.cpp
@@ -27,10 +27,47 @@ int main()
 	sineGenerator4.setAmplitude(60).setFrequency(f2).setPhase(0.4).generate(SIZE);
 
 	auto sum = sineGenerator4 + sineGenerator3 + sineGenerator2 + sineGenerator1;
+	auto other_sum = sineGenerator4 + sineGenerator3 + sineGenerator2 + sineGenerator1;
 
 	Aquila::TextPlot plt("Signal waveform before filtration");
 	plt.plot(sum);
 
 	// just get this much working.
+
+	// where does fft Get its freq values from???
+
+	auto fft = Aquila::FftFactory::getFft(SIZE);
+	Aquila::SpectrumType spectrum = fft->fft(sum.toArray());
+	plt.setTitle("Signal spectrum before filtration");
+	plt.splotSpectrum(spectrum);
+
+	Aquila::SpectrumType inverse(SIZE);
+	for (std::size_t i = 0; i < SIZE; ++i) {
+		inverse[i] = -1;
+	}
+
+	plt.setTitle("inverse Spectrum");
+	plt.plotSpectrum(inverse);
+
+	std::transform(
+		std::begin(spectrum),
+		std::end(spectrum),
+		std::begin(inverse),
+		std::begin(spectrum),
+		[] (Aquila::ComplexType x, Aquila::ComplexType y) {return x * y}
+	);
+	plt.setTitle("Flipped Signal");
+	plt.plotSpectrum(spectrum);
+
+	// back to time-domain
+	double flip_sig[SIZE];
+	fft->ifft(spectrum, flip_sig);
+	plt.setTitle("Signal after flipping");
+	plt.plot(flip_sig, SIZE);
+
+	// sum norm and flipped versions; 0? Does it add frequency spectra?
+	auto final = flip_sig + other_sum;
+	plt.setTitle("Cancelled noise?");
+	plt.plot(final);
 
 }
