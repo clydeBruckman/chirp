8a5ef8e06b9f09f25d4772f2b504d29c9496befb
Jack Lynch <jacklynchtds@gmail.com>
2016-04-23 17:05:08 -0400

    further developed test program

--- testboard.ino
+++ testboard.ino
@@ -21,18 +21,22 @@ AudioConnection          patchCord5(filter1, 0, fft1024_1, 0);
 AudioConnection          patchCord6(filter2, 0, fft1024_2, 0);
 // GUItool: end automatically generated code
 
-double peak_1_scaled;
-double peak_2_scaled;
+// ** question for PJRC
+// *  question for Michael
+//    question for me
+
+// fuck I just realized you need to implement GPS and a coordinate system...
+
+double scale_factor_1;
 
 void setup() {
 
 	AudioMemory(12); // ** how do I choose how much audio memory?
 
-	fft1024_1.WindowFunction(AudioWindowHanning1024);
-	fft1024_2.WindowFunction(AudioWindowHanning1024);
+	fft1024_1.WindowFunction(AudioWindowHanning1024); // which window?
+	fft1024_2.WindowFunction(AudioWindowHanning1024); // http://electronicdesign.com/analog/choose-right-fft-window-function-when-evaluating-precision-adcs
 
-	peak_1_scaled = 0;
-	peak_2_scaled = 0;
+	scale_factor_1 = 0;
 
 }
 
@@ -40,12 +44,19 @@ void loop() {
 
 	// FIRST: SCALE PEAKS
 	if (peak1.available() && peak2.available()) {
-		peak_1_scaled *= (peak2.read() / peak1.read()); // ratio?
-		peak_2_scaled = peak2.read();
-	}
+		scale_factor_1 *= (peak1.read() / peak2.read()); // ratio?
+	} // peak, or peak-to-peak? I'm thinking the second...
 
-	if (fft1024_1.available()) {
-
+	if (fft1024_1.available() && fft1024_2.available()) { // if these have 500+ bins why tf does the ex code only cycle through 40???
+		for(int i = 0; i <= 511; i++){ // scaling freq bins bad idea!
+			if ( abs(fft1024_1.read(i)*scale_factor_1 + fft1024_2.read(i)) > 0.1 ) { // replace .01 with "SIGNIFICANT" defined static var.
+				// something! // here's the "GPS and coordinate system" stuff
+				//https://www.pjrc.com/teensy/td_libs_TinyGPS.html
+				//https://forum.pjrc.com/threads/24979-Teensy-3-1-Ultimate-GPS-code
+				//should I connect Teensy to Arduino via digital ins and outs, and make Arduino handle the GPS code?
+				//why not
+			} //  >> IS IT BAD TO SCALE FREQ BINS??? QUITE POSSIBLY!!!!! <<
+		} 
 	}
 
 	// SCALE FFT1 TO MATCH FFT2, THEN INVERT 1 AND ADD TOGETHER. THEN ANALYZE
